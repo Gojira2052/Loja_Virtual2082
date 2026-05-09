0339b30e64e8e0bc2c42497bc29e0522eb174aea
Daniel <danielvalente1104@gmail.com>
2026-05-09 14:04:25 -0300

    loja

---
+++ estruturas_condiocionais.php
@@ -0,0 +1,11 @@
+<?php
+$temperatura = 19;
+if (temperatura > 30) {
+    echo "🥤 Está muito quente!";
+} 
+elseif(temperatura > 20) {
+    echo "😎 Temperatura agradável.";
+} 
+else {
+    echo "💀 Frio Lazarento!";
+}
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+require 'config.php';
+
+// Buscar produtos no banco de dados
+
+$tmt = $pdo->query("SELECT * FROM produtos ORDER BY criado_em DESC");
+$produtos = $stmt->fetchAll();
+
+// Lógica simples para adicionar ao carrinho
+
+if (isset($_GET['adicionar'])) {
+    $id_produto = (int)$_GET['adicionar'];
+    if (!isset($_SESSION['carrinho'])) {
+        $_SESSION['carrinho'] = [];
+    }
+}
+// Adicionar o ID ao carrinho
+
+$_SESSION['carrinho'][] = $id_produto;
+header("Location: index.php?sucesso=1");
+exit;
+}   
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Loja PHP - Ecommerce</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Minha Loja PHP</h1>
+        <nav>
+            <a href="index.php">Início</a>
+            <a href="carrinho.php">
+                Carrinho (<?php echo isset($_SESSION['carrinho']) ? count($_SESSION['carrinho']) : 0; ?>)
+            </a>
+            <?php if (isset($_SESSION['usuario_id'])): ?>
+            <a href="cadastro_produto.php">Cadastrar Produto</a>
+            <a href="logout.php">Sair (<?php echo $_SESSION['usuario_nome']; ?>)</a>
+            <?php else: ?>
+            <a href="login.php">Login</a>
+            <?php endif; ?>
+        </nav>
+        
+    </header>
